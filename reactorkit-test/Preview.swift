0096e0599cc2fc0f0094bee8ef34967027737e64
Skyline-23 <bs2740@naver.com>
2021-07-26 12:47:17 +0900

    [ADD] preview에 필요한 파일들 추가

---
+++ reactorkit-test/Preview.swift
@@ -0,0 +1,17 @@
+//
+//  Preview.swift
+//  reactorkit-test
+//
+//  Created by 김부성 on 2021/07/25.
+//
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+struct ViewControllerPreview: PreviewProvider {
+    static var previews: some View {
+        ViewController().showPreview(.iPhone12Pro)
+    }
+}
+
+#endif
